e50707ddf58611c61fd64c2487862b749c08de92
Tami Takamiya <ttakamiy@redhat.com>
2024-10-10 16:54:08 -0400

    parse url

--- src/parse-aap-docs.py
+++ src/parse-aap-docs.py
@@ -2,21 +2,51 @@ import re
 from pathlib import Path
 
 
-AAP_DOCS = "/home/ttakamiy/git/ansible/aap-docs"
-
 class ParseAdocs:
 
     def __init__(self, base_dir):
         self.base_dir = base_dir
 
+
     def run(self):
-        self.adocs_dict = self.get_dict()
         self.parse_attributes()
+        self.adocs_dict = self.get_dict()
+        self.parse_doc_info_files()
         self.parse_include()
+
+        # for k,v in self.adocs_dict.items():
+        #     print(v)
+
 
     def get_adocs(self):
         return list(
             filter(lambda x: "/archive/" not in str(x), Path(self.base_dir).joinpath("downstream").rglob("*.adoc")))
+
+    def parse_doc_info_files(self):
+        count = 0
+        title_pattern = re.compile(r"^\s*<title>(.+)</title>\s*$")
+        docinfo_files = list(filter(lambda x: "/archive/" not in str(x), Path(self.base_dir).joinpath("downstream").rglob("docinfo.xml")))
+        for docinfo in docinfo_files:
+            for line in open(docinfo):
+                m = title_pattern.match(line)
+                if m:
+                    title = m.group(1).strip()
+                    if title in self.title_dict:
+                        pass
+                        # print(f"{title} -> {self.title_dict[title]}")
+                        dir_name = str(docinfo.parents[0])
+                        i = dir_name.index("/downstream/")
+                        master_adoc = f"{dir_name[i:]}/master.adoc"
+                        if master_adoc not in self.adocs_dict:
+                            print(f"NOT FOUND: {master_adoc}")
+                        else:
+                            url = self.attributes_dict["URL" + self.title_dict[title]]
+                            self.adocs_dict[master_adoc]["url"] = url
+                    else:
+                        print(f"{title} IS NOT FOUND")
+                    count += 1
+        return
+
 
     def get_dict(self):
         d = {}
@@ -24,7 +54,13 @@ class ParseAdocs:
             path_name = str(adoc)
             i = path_name.find("/downstream/")
             project_file_name = path_name[i:]
-            d[project_file_name] = { "path_name": path_name }
+            d[project_file_name] = {
+                "path_name": path_name,
+                "includes": set(),
+                "included_by": set(),
+                "broken_links": set(),
+                "url": None,
+            }
         return d
 
 
@@ -55,25 +91,30 @@ class ParseAdocs:
 
         if not found:
             print(f"NOT FOUND: {include_file} (source: {source})")
+            file_name = None
 
-        return found
+        return file_name
 
 
     def parse_include(self):
         not_found = 0
         include_pattern = re.compile(r"^\s*include::([\w\./\-_]+).*$")
         for k,v in self.adocs_dict.items():
-            # print(f"SOURCE: {k}")
             for line in open(v["path_name"]):
                 m = include_pattern.match(line)
                 if m:
                     include_file = m.group(1)
                     include_file = self.substitude_attributes(include_file)
-                    # print(f"include_file={include_file}")
-                    found = self.find_include_file(k, include_file)
-                    if not found:
+                    file_name = self.find_include_file(k, include_file)
+                    if file_name is None:
                         not_found += 1
+                        v["broken_links"].add(include_file)
+                    else:
+                        v["includes"].add(file_name)
+                        if file_name in self.adocs_dict:
+                            self.adocs_dict[file_name]["included_by"].add(k)
         print(f"NOT FOUND: {not_found}")
+
 
     def substitude_attributes(self, line):
         pattern = re.compile(r"{(\w+)}")
@@ -85,8 +126,10 @@ class ParseAdocs:
                 print(f"Attribute {attribute} is not found.")
         return line
 
+
     def parse_attributes(self):
         self.attributes_dict = {}
+        self.title_dict = {}
         pattern = re.compile(r":(\w+):\s*(.+)$")
         menu_pattern = re.compile(r"^menu:([\w ]+)\[([\w >]+)\]$")
         attributes_adoc = Path(self.base_dir).joinpath("downstream").joinpath("attributes").joinpath("attributes.adoc")
@@ -101,11 +144,13 @@ class ParseAdocs:
                 m = menu_pattern.match(value)
                 if m:
                     value = f"{m.group(1)} > {m.group(2)}"
-                # print(key, value)
                 self.attributes_dict[key] = value
+                if key.startswith("Title"):
+                    self.title_dict[value] = key[len("Title"):]
 
 
 def main():
+    AAP_DOCS = "/home/ttakamiy/git/ansible/aap-docs"
     ParseAdocs(AAP_DOCS).run()
 
 if __name__ == "__main__":
