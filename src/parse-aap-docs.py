7a7d0c8426c708c2f9b1783150009e48b2bcba67
Tami Takamiya <ttakamiy@redhat.com>
2024-10-09 20:59:38 -0400

    Find include file

--- src/parse-aap-docs.py
+++ src/parse-aap-docs.py
@@ -6,7 +6,7 @@ AAP_DOCS = "/home/ttakamiy/git/ansible/aap-docs"
 
 
 def get_adocs(base_dir):
-    return list(filter(lambda x: "/archives/" not in str(x), Path(base_dir).joinpath("downstream").rglob("*.adoc")))
+    return list(filter(lambda x: "/archive/" not in str(x), Path(base_dir).joinpath("downstream").rglob("*.adoc")))
 
 def get_dict(adocs):
     d = {}
@@ -17,16 +17,54 @@ def get_dict(adocs):
         d[project_file_name] = { "path_name": path_name }
     return d
 
+def find_include_file(adocs_dict, source, include_file):
+    found = False
+
+    file_name = f"/downstream/{include_file}"
+    found = file_name in adocs_dict
+
+    if not found:
+        if include_file.startswith("../"):
+            file_name = f"/downstream/{include_file[3:]}"
+            found = file_name in adocs_dict
+
+    if not found:
+        i = source.rfind("/")
+        if include_file.startswith("../"):
+            i = source[:i].rfind("/")
+            file_name = f"{source[:i]}/{include_file[3:]}"
+        else:
+            file_name = f"{source[:i]}/{include_file}"
+        found = file_name in adocs_dict
+
+    if not found:
+        for subdirectory in ["assemblies", "modules"]:
+            file_name = f"/downstream/{subdirectory}/{include_file}"
+            found = file_name in adocs_dict
+            if found:
+                break
+
+    if not found:
+        print(f"NOT FOUND: {include_file} (source: {source})")
+
+    return found
+
+
 def parse_include(adocs_dict, attributes_dict):
+    not_found = 0
     include_pattern = re.compile(r"^\s*include::([\w\./\-_]+).*$")
     for k,v in adocs_dict.items():
-        print(f"SOURCE: {k}")
+        # print(f"SOURCE: {k}")
         for line in open(v["path_name"]):
             m = include_pattern.match(line)
             if m:
                 include_file = m.group(1)
                 include_file = substitude_attributes(attributes_dict, include_file)
-                print(f"include_file={include_file}")
+                # print(f"include_file={include_file}")
+                found = find_include_file(adocs_dict, k, include_file)
+                if not found:
+                    not_found += 1
+    print(f"NOT FOUND: {not_found}")
 
 def substitude_attributes(attributes_dict, line):
     pattern = re.compile(r"{(\w+)}")
