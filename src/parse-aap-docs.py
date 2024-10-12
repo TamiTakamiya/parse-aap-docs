a3a63a143c04fc78163295f48e21956a6a9757c8
Tami Takamiya <ttakamiy@redhat.com>
2024-10-12 15:22:16 -0400

    Refactor

--- src/parse-aap-docs.py
+++ src/parse-aap-docs.py
@@ -10,17 +10,30 @@ import requests
 class ParseAAPDocs:
     """
     Parse the aap-docs repository and locate Red Hat product document URLs for each
-    source Asciidoc files so that they can be used as referenced document links
+    source Asciidoc file so that they can be used as referenced document links
     displayed on the chatbot UI.
     """
 
     def __init__(self, base_dir, project_document_path, files_to_skip, do_validate=False):
+        """
+        Initialize the ParseAAPDocs object
+
+        :param base_dir: aap-docs local repository
+        :param project_document_path: a relative path where Asciidoc sources are stored
+        :param files_to_skip: a list of Ascii files paths to be ignored in the processing
+        :param do_validate: whether a validation of URL is executed or not
+        """
         self.base_dir = base_dir
         self.project_document_path = project_document_path
         self.files_to_skip = files_to_skip
         self.do_validate = do_validate
 
     def run(self):
+        """
+        Analyze the local aap-docs repository and calculate URLs for each Ascii file
+
+        :return: None
+        """
         self.parse_attributes()
         self.adocs_dict = self.get_dict()
         self.parse_doc_info_files()
@@ -32,6 +45,12 @@ class ParseAAPDocs:
         print(f"total: {len(self.adocs_dict)}")
 
     def validate(self, adoc):
+        """
+        Validate the URL given to a specified Asciidoc is a valid documentation URL
+
+        :param adoc:
+        :return: whether the URL is valid or not.
+        """
         url = adoc["url"]
         try:
             response = requests.get(url, allow_redirects=False)
@@ -40,12 +59,17 @@ class ParseAAPDocs:
             else:
                 print(f"INVALID: {url} status_code={response.status_code}")
                 return False
-        except requests.ConnectionError as exception:
+        except requests.ConnectionError:
             print(f"INVALID: {url} ConnectionError")
             return False
 
     def parse_title_docs(self):
-        title_docs = list(filter(lambda x: self.adocs_dict[x]["url"] != None, self.adocs_dict))
+        """
+        Parse title documents
+
+        :return: None
+        """
+        title_docs = list(filter(lambda x: self.adocs_dict[x]["url"] is not None, self.adocs_dict))
         for title_doc in title_docs:
             if title_doc in self.files_to_skip:
                 self.adocs_dict[title_doc]["url"] = None
@@ -64,6 +88,15 @@ class ParseAAPDocs:
             self.simulate_includes(self.adocs_dict[title_doc], context)
 
     def simulate_includes(self, adoc, context):
+        """
+        Simulate Asciidoc include operation. Note that this code does not read files line by
+        line like a real parser. So the results might be different from the ones obtained from
+        a real Asciidoc parser.
+
+        :param adoc: An Asciidoc to be parsed
+        :param context: The current context
+        :return: None
+        """
         id = None
         nested_assembly = adoc["nested_assembly"]
         context_save = copy.copy(context) if nested_assembly else None
@@ -104,6 +137,12 @@ class ParseAAPDocs:
             context["url"] = context_save["url"]
 
     def get_adocs(self):
+        """
+        Get the list of Ascii documents in the aap-docs local repo. Ignore files under
+        /archive/ folder.
+
+        :return:  None.
+        """
         return list(
             filter(
                 lambda x: "/archive/" not in str(x),
@@ -112,6 +151,13 @@ class ParseAAPDocs:
         )
 
     def parse_doc_info_files(self):
+        """
+        Parse docinfo.xml file to get titles of title Ascii docs. Since Python ElementTree
+        XML API did not work well, a simple regex is used and it seems good enough for our
+        purpose.
+
+        :return: None (self.adoc_dict is updated).
+        """
         count = 0
         title_pattern = re.compile(r"^\s*<title>(.+)</title>\s*$")
         docinfo_files = list(
@@ -137,9 +183,13 @@ class ParseAAPDocs:
                     else:
                         print(f"{title} IS NOT FOUND")
                     count += 1
-        return
 
     def parse_adoc(self, adoc_path):
+        """
+        Parse an Ascii document.
+        :param adoc_path: A path to an Ascii document.
+        :return: id, context, content_type, nested_assembly
+        """
         id = None
         id_pattern = re.compile(r'\[id=["\']([^"\']+)["\']\]')
         context = None
@@ -166,6 +216,11 @@ class ParseAAPDocs:
         return id, context, content_type, nested_assembly
 
     def get_dict(self):
+        """
+        Initialize a dict to store the information on Ascii docs.
+
+        :return: A dict
+        """
         d = {}
         for adoc in self.get_adocs():
             path_name = str(adoc)
@@ -185,6 +240,15 @@ class ParseAAPDocs:
         return d
 
     def find_include_file(self, source, include_file):
+        """
+        Locate files included. aap-docs repository have symbolic links for organizing
+        include files. This code uses Python File I/O function to resolve target
+        directories pointed by symbolic links.
+
+        :param source:
+        :param include_file:
+        :return: target file
+        """
         base_path = Path(self.base_dir)
         parent_path = base_path.joinpath(Path(source).parents[0])
         include_file_path = parent_path.joinpath(include_file)
@@ -197,18 +261,29 @@ class ParseAAPDocs:
         return file_name
 
     def parse_include(self):
+        """
+        Parse include lines in Ascii files.
+
+        :return: None (self.adocs_dict is updated).
+        """
         include_pattern = re.compile(r"^\s*include::([\w\./\-_]+).*$")
         for k, v in self.adocs_dict.items():
             for line in open(v["path_name"]):
                 m = include_pattern.match(line)
                 if m:
                     include_file = m.group(1)
-                    include_file = self.substitude_attributes(include_file)
+                    include_file = self.substitute_attributes(include_file)
                     file_name = self.find_include_file(k, include_file)
                     if file_name:
                         v["includes"].append(file_name)
 
-    def substitude_attributes(self, line):
+    def substitute_attributes(self, line):
+        """
+        Substitude attributes in a line to the defined values.
+
+        :param line: string to be parsed
+        :return: Modified string
+        """
         pattern = re.compile(r"{(\w+)}")
         attributes = pattern.findall(line)
         for attribute in attributes:
@@ -219,6 +294,11 @@ class ParseAAPDocs:
         return line
 
     def parse_attributes(self):
+        """
+        Parse attributes.adoc
+
+        :return: None (self.attributes_dict and self.title_dict are updated).
+        """
         self.attributes_dict = {}
         self.title_dict = {}
         pattern = re.compile(r":(\w+):\s*(.+)$")
@@ -236,7 +316,7 @@ class ParseAAPDocs:
             m = pattern.match(line)
             if m:
                 key = m.group(1)
-                value = self.substitude_attributes(m.group(2))
+                value = self.substitute_attributes(m.group(2))
                 m = menu_pattern.match(value)
                 if m:
                     value = f"{m.group(1)} > {m.group(2)}"
